becdd29b973d86feaa44b3cfcdab97930f8382bb
petykokusz79 <petykokusz.79@gmail.com>
2024-11-01 18:44:43 +0100

    Implement tests of TorpedoStore

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,9 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +19,32 @@ class TorpedoStoreTest {
 
         // Assert
         assertEquals(true, result);
+    }
+
+    @Test
+    void empty() {
+        TorpedoStore store = new TorpedoStore(0);
+        assertEquals(0, store.getTorpedoCount());
+        assertTrue(store.isEmpty());
+    }
+
+    @Test
+    void notEmpty() {
+        TorpedoStore store = new TorpedoStore(1);
+        assertEquals(1, store.getTorpedoCount());
+        assertFalse(store.isEmpty());
+    }
+    
+    @Test
+    void failure() {
+        TorpedoStore store = new TorpedoStore(1, 1);
+        assertFalse(store.fire(1));
+    }
+    
+    @Test
+    void numberOutOfBound() {
+        TorpedoStore store = new TorpedoStore(1);
+        assertThrows(IllegalArgumentException.class, () -> store.fire(0));
+        assertThrows(IllegalArgumentException.class, () -> store.fire(2));
     }
 }
